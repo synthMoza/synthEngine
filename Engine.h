55b4351f5701f240beb098f383ea5e423000a652
Еганян Алексей <eganian.aa@phystech.edu>
2021-04-03 13:53:43 +0300

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -2,7 +2,25 @@
 
 // File to be included, contains each component of the engine
 
+// Sketches
 #include "SketchDriver/BasicSketch.h"
 #include "SketchDriver/Sketch.h"
 #include "SketchDriver/SketchDriver.h"
+
+// Application
 #include "Application/Application.h"
+
+// Graphics
+#include "GraphicalDriver/GraphicalObject.h"
+#include "GraphicalDriver/GraphicalDriver.h"
+#include "GraphicalDriver/Color.h"
+#include "GraphicalDriver/Shapes/CircleShape.h"
+
+// Input
+#include "InputDriver.h"
+
+// Time
+#include "TimeDriver.h"
+
+// Utilities
+#include "Utilities/Vector2.h"
